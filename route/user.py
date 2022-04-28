9a2d2fe8fd7100b9940364b0b1216ac3786c8dfe
Wjong <88592432+www-jong@users.noreply.github.com>
2022-04-28 14:41:08 +0900

    dd

--- route/user.py
+++ route/user.py
@@ -224,15 +224,11 @@ def aiservice():
             print('성장도 :',float(str(y_scaler.inverse_transform(norm_preds))[2:7]))
             print('성장률 :',preds)
             print(lin_imgsize)
-<<<<<<< HEAD
             return render_template('alert/learning_complate.html',
                                    pred_result=str(preds)[0:4]+"%",
                                    now_size=float(str(lin_imgsize)[2:8]),
                                    growed=float(str(y_scaler.inverse_transform(norm_preds))[2:7]),
                                    mask_img=mask_img)
-=======
-            return render_template('user/aiservice_pred.html')
->>>>>>> 6be3504315b3941accd4dfec0b54955404c478d0
     else:#세션정보 있을시, 
         return redirect(url_for('login'))
     
